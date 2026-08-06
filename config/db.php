213929aa4bcace20d16ceb6b8b9fe5a1a3eaad5f
Gustavo Muller <gustavo.muller@sap.com>
2026-08-05 23:45:41 -0300

    build & dependency changes

--- config/db.php
+++ config/db.php
@@ -1,15 +1,14 @@
-
 <?php
 /**
- * @copyright Copyright (c) 2022 RNP
+ * @copyright Copyright (c) 2019 RNP
  * @license http://github.com/ufrgs-hyman/meican#license
  */
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=meican31',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=meican-db;dbname=your_database_name',
+    'username' => 'your_user',
+    'password' => 'your_password',
     'charset' => 'utf8',
     'tablePrefix' => 'meican_',
 ];
